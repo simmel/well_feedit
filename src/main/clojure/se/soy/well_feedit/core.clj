9f3dc2ed449e316160bd9a4d4e41929f16b42fd5
Simon Lundström <github-commits@soy.se>
2020-05-25 22:52:56 +0200

    Add buran RSS/ATOM feed dep and fetch feed

--- src/main/clojure/se/soy/well_feedit/core.clj
+++ src/main/clojure/se/soy/well_feedit/core.clj
@@ -1,8 +1,23 @@
 (ns se.soy.well_feedit.core
   (:gen-class)
+  (:require [
+             buran.core :refer [
+                                combine-feeds
+                                consume
+                                consume-http
+                                filter-entries
+                                produce
+                                shrink
+                                sort-entries-by
+                                ]
+             ]
+            )
   )
 
-(defn -main [& args]
-  (do (println "lol")
-      )
-  )
+;; (defn -main [& args]
+;;   (do (println "lol")
+;;       )
+;;   )
+;;
+
+(def f (consume-http {:from "https://soy.se/netsec.atom"}))
