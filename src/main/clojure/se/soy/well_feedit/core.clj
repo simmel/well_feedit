57999c3beecf37bed8a3296d11c469debf776ee5
Simon Lundström <github-commits@soy.se>
2020-05-27 09:14:55 +0200

    Apparently you functions need to exist when defn
    And they did exist in the REPL since I had already loaded them before, I
    guess.

--- src/main/clojure/se/soy/well_feedit/core.clj
+++ src/main/clojure/se/soy/well_feedit/core.clj
@@ -22,38 +22,6 @@
 
 (def f (consume-http {:from "https://soy.se/netsec.atom"}))
 
-(defn update-link [entry]
-  (update
-    entry
-    :link
-    get-original-url
-    entry
-    )
-  )
-
-; FIXME Merge these into one function with different argument arities?
-(defn update-comment-link [entry]
-  (update
-    entry
-    :links
-    update-comment-links
-    entry
-    )
-  )
-
-(defn update-comment-links [link entry]
-  (map #(assoc %
-               :rel "replies"
-               :type "text/html"
-               :href (get-comment-url entry)
-               )
-       link)
-  )
-
-(defn remove-content [entry]
-  (dissoc entry :contents)
-  )
-
 (defn get-content-urls [entry]
   (map (fn [a] (second a))
        (rest
@@ -71,3 +39,35 @@
 
 (defn get-original-url [_ entry] (first (get-content-urls entry)))
 (defn get-comment-url [entry] (last (get-content-urls entry)))
+
+(defn update-link [entry]
+  (update
+    entry
+    :link
+    get-original-url
+    entry
+    )
+  )
+
+; FIXME Merge these into one function with different argument arities?
+(defn update-comment-links [link entry]
+  (map #(assoc %
+               :rel "replies"
+               :type "text/html"
+               :href (get-comment-url entry)
+               )
+       link)
+  )
+
+(defn update-comment-link [entry]
+  (update
+    entry
+    :links
+    update-comment-links
+    entry
+    )
+  )
+
+(defn remove-content [entry]
+  (dissoc entry :contents)
+  )
