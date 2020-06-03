05f9c9db3b2dc5a086c796ae21141fac81064043
Simon Lundström <github-commits@soy.se>
2020-06-03 21:51:46 +0200

    2.0.0

--- src/main/clojure/se/soy/well_feedit/core.clj
+++ src/main/clojure/se/soy/well_feedit/core.clj
@@ -19,7 +19,7 @@
 
 (def version {
               :name "well_feedit"
-              :version "1.0.1"
+              :version "2.0.0"
               :url "https://github.com/simmel/well_feedit/"
               }
   )
