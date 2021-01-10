814db8dc81469e8de7604db14eb83999bfdd849e
Simon Lundström <simlu+github@su.se>
2021-01-10 12:48:11 +0100

    Add a very simple test, which fails

---
+++ src/test/clojure/se/soy/well_feedit/core_test.clj
@@ -0,0 +1,10 @@
+(ns se.soy.well_feedit.core_test
+  (:require [clojure.test :refer :all]
+            ))
+
+(deftest a-test
+         (testing "Testing is"
+                  (is (= 1 1))
+                  (is (= 1 0))
+                  )
+         )
